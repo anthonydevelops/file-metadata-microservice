13af2e1b202f51a5abf42d6155c96b85db21f5c3
anthonydevelops <anthonydevelops@gmail.com>
2018-10-06 23:04:25 -0700

    Removed unnecessary call to /api

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,6 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-  state = {
-    response: ""
-  };
-
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-    const response = await fetch("/api");
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
   render() {
     return (
       <div className="App">
